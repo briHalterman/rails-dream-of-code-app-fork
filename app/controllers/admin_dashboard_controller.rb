fa9de8c83326ee4c2a4eea87c936469dad8e4460
Brittany Halterman <halterma@gmail.com>
2025-05-30 22:05:19 -0400

    Display the next upcoming trimester
    -Create a new instance variable and set it to the upcoming trimester
    -Update the view to retrieve the upcoming trimester's term and year

--- app/controllers/admin_dashboard_controller.rb
+++ app/controllers/admin_dashboard_controller.rb
@@ -1,5 +1,6 @@
 class AdminDashboardController < ApplicationController
   def index
     @current_trimester = Trimester.where('start_date <= ?', Date.today).where('end_date >= ?', Date.today).first
+    @upcoming_trimester = Trimester.where('start_date > ?', Date.today).where('start_date <= ?', Date.today + 6.months).where('end_date >= ?', Date.today).first
   end
 end
